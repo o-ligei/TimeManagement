6c9dc033265559705e7d34db630636e7030a4c14
feax <48175749+feaaaaaa@users.noreply.github.com>
2020-09-14 18:11:52 +0800

    Initial Commit (#1)
    Signed-off-by: feaaaaaa <409834725@qq.com>

---
+++ app/src/main/java/com/example/wowtime/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.wowtime;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, AnotherActivity.class)));
+        Button button2 = findViewById(R.id.button3);
+        button2.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, TestActivity.class)));
+    }
+}
