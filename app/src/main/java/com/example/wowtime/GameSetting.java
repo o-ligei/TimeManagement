be1af1c3b161f7488fae4eb2e4b5d0edf05a8248
mark-yyc <61784497+mark-yyc@users.noreply.github.com>
2020-09-22 18:09:21 +0800

    feat: clock pages complete

--- app/src/main/java/com/example/wowtime/GameSetting.java
+++ app/src/main/java/com/example/wowtime/GameSetting.java
@@ -9,22 +9,22 @@ import com.example.wowtime.adapter.RingItemAdapter;
 
 import java.util.ArrayList;
 
-public class RingSetting extends AppCompatActivity {
+public class GameSetting extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ring_setting);
-
+        setContentView(R.layout.activity_game_setting);
 
         ArrayList<String> arr=new ArrayList<>();
-        for (int i=0;i<20;i++){
-            String title="ring"+ i;
+        for (int i=0;i<5;i++){
+            String title="game"+ i;
             arr.add(title);
         }
+
         RingItemAdapter adapter=new RingItemAdapter(arr,getApplicationContext());
 //        ArrayAdapter<String> adapter=new ArrayAdapter<>(this,R.layout.activity_ring_list_item,arr);
-        ListView listView = findViewById(R.id.RingSettingList);
+        ListView listView = findViewById(R.id.GameSettingList);
         listView.setAdapter(adapter);
     }
 }
