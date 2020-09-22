27d8f98e430a401ddad8fa72d2f8de394a1e58f3
August <46640505+ShaoJieCui@users.noreply.github.com>
2020-09-22 21:58:32 +0800

    fix:white list layout

--- app/src/main/java/com/example/wowtime/WhiteListActivity.java
+++ app/src/main/java/com/example/wowtime/WhiteListActivity.java
@@ -5,19 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
-import com.example.wowtime.adapter.AppAdapter;
+import com.example.wowtime.adapter.AppItemAdapter;
 
 import java.util.LinkedList;
-import java.util.List;
-import android.os.Bundle;
-import android.widget.ListView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.wowtime.adapter.AppAdapter;
 import com.example.wowtime.component.AppListItem;
-
-import java.util.LinkedList;
 
 public class WhiteListActivity extends AppCompatActivity {
 
@@ -31,9 +23,9 @@ public class WhiteListActivity extends AppCompatActivity {
         appListItems.add(new AppListItem("WeChat",R.drawable.wechat));
         appListItems.add(new AppListItem("QQ",R.drawable.qq));
 
-        AppAdapter appAdapter = new AppAdapter(appListItems,getApplicationContext());
+        AppItemAdapter appItemAdapter = new AppItemAdapter(appListItems,getApplicationContext());
 
         ListView listView = (ListView) findViewById(R.id.white_list);
-        listView.setAdapter(appAdapter);
+        listView.setAdapter(appItemAdapter);
     }
 }
