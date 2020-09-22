c72925efac628a98218e5828eb71995685463501
mark-yyc <61784497+mark-yyc@users.noreply.github.com>
2020-09-22 19:44:47 +0800

    feat: clock page relative (#13)
    * feat: clock setting page
    
    * feat;ring setting
    
    * feat: clock pages complete

--- app/src/main/java/com/example/wowtime/WhiteListActivity.java
+++ app/src/main/java/com/example/wowtime/WhiteListActivity.java
@@ -32,7 +32,6 @@ public class WhiteListActivity extends AppCompatActivity {
         appListItems.add(new AppListItem("QQ",R.drawable.qq));
 
         AppAdapter appAdapter = new AppAdapter(appListItems,getApplicationContext());
-
         ListView listView = (ListView) findViewById(R.id.white_list);
         listView.setAdapter(appAdapter);
     }
