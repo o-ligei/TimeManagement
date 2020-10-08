4f71eefc9114bb3f2104f57b66295eca0f305f7e
feax <48175749+feaaaaaa@users.noreply.github.com>
2020-10-08 13:30:12 +0800

    feat: part of navigation & fragment of view
    Signed-off-by: feaaaaaa <409834725@qq.com>

--- app/src/main/java/com/example/wowtime/TaskListActivity.java
+++ app/src/main/java/com/example/wowtime/TaskListActivity.java
@@ -1,8 +1,18 @@
 package com.example.wowtime;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.example.wowtime.adapter.TaskItemAdapter;
@@ -12,22 +22,52 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class TaskListActivity extends AppCompatActivity {
+//public class TaskListActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_task_list);
+//
+//        ArrayList<TaskListItem> taskListItems = new ArrayList<>();
+//
+//        taskListItems.add(new TaskListItem("快速算算算"));
+//        taskListItems.add(new TaskListItem("使劲摇摇摇"));
+//        taskListItems.add(new TaskListItem("努力吹吹吹"));
+//
+//        TaskItemAdapter taskItemAdapter = new TaskItemAdapter(taskListItems,getApplicationContext());
+//
+//        ListView listView = (ListView) findViewById(R.id.task_list);
+//        listView.setAdapter(taskItemAdapter);
+//    }
+//}
+public class TaskListActivity extends Fragment{
+    public TaskListActivity() {
+    }
+
+    public TaskListActivity(int contentLayoutId) {
+        super(contentLayoutId);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_task_list);
+    }
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,@Nullable ViewGroup container,@Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.activity_task_list, container, false);
         ArrayList<TaskListItem> taskListItems = new ArrayList<>();
 
         taskListItems.add(new TaskListItem("快速算算算"));
         taskListItems.add(new TaskListItem("使劲摇摇摇"));
         taskListItems.add(new TaskListItem("努力吹吹吹"));
 
-        TaskItemAdapter taskItemAdapter = new TaskItemAdapter(taskListItems,getApplicationContext());
+        TaskItemAdapter taskItemAdapter = new TaskItemAdapter(taskListItems,getContext());
 
-        ListView listView = (ListView) findViewById(R.id.task_list);
+        ListView listView = root.findViewById(R.id.task_list);
         listView.setAdapter(taskItemAdapter);
+        return root;
     }
 }
