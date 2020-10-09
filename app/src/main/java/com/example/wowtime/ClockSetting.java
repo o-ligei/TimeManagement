58af83d0f8b9ce508737b575d7d48c03045ee52c
feaaaaaa <409834725@qq.com>
2020-10-09 16:49:45 +0800

    fix:adjust jump
    Signed-off-by: feaaaaaa <409834725@qq.com>

--- app/src/main/java/com/example/wowtime/ClockSetting.java
+++ app/src/main/java/com/example/wowtime/ClockSetting.java
@@ -2,7 +2,9 @@ package com.example.wowtime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ClockSetting extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class ClockSetting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clock_setting);
+        Button button=findViewById(R.id.button8);
+        button.setOnClickListener(v->startActivity(new Intent(ClockSetting.this,TaskListActivity.class)));
     }
 }
