316b4498a2f94addf4f58048af6c465a12d9f38b
feaaaaaa <409834725@qq.com>
2020-10-18 16:25:07 +0800

    feat: primary float window
    Signed-off-by: feaaaaaa <409834725@qq.com>

--- android/app/src/main/java/com/example/wowtime/ui/pomodoro/PomodoroSettingActivity.java
+++ android/app/src/main/java/com/example/wowtime/ui/pomodoro/PomodoroSettingActivity.java
@@ -1,15 +1,25 @@
 package com.example.wowtime.ui.pomodoro;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.NumberPicker;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.wowtime.MainApplication;
 import com.example.wowtime.R;
 
 import java.util.ArrayList;
@@ -17,6 +27,9 @@ import java.util.List;
 
 
 public class PomodoroSettingActivity extends AppCompatActivity {
+
+//    private FloatView mFloatView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -78,5 +91,64 @@ public class PomodoroSettingActivity extends AppCompatActivity {
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(100, FrameLayout.LayoutParams.WRAP_CONTENT);
         params.setMargins(10, 0, 10, 0);
         np.setLayoutParams(params);
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void startFloatingImageDisplayService(View view) {
+        if (FloatingImageDisplayService.isStarted) {
+            return;
+        }
+        if (!Settings.canDrawOverlays(this)) {
+            Toast.makeText(this, "当前无权限，请授权", Toast.LENGTH_SHORT).show();
+            startActivityForResult(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName())), 1);
+        } else {
+            startService(new Intent(PomodoroSettingActivity.this, FloatingImageDisplayService.class));
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "授权失败", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "授权成功", Toast.LENGTH_SHORT).show();
+                startService(new Intent(PomodoroSettingActivity.this, FloatingImageDisplayService.class));
+            }
+        } else if (requestCode == 1) {
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "授权失败", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "授权成功", Toast.LENGTH_SHORT).show();
+                startService(new Intent(PomodoroSettingActivity.this, FloatingImageDisplayService.class));
+            }
+        } else if (requestCode == 2) {
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "授权失败", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "授权成功", Toast.LENGTH_SHORT).show();
+                startService(new Intent(PomodoroSettingActivity.this, FloatingImageDisplayService.class));
+            }
+        }
+    }
+
+
+    // 如下代码直接跳到该应用的系统权限设置页面
+    private Boolean requestOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, 111);
+                return false;
+            } else {
+                System.out.println("OVERLAY_PERMISSION:OK");
+                return true;
+            }
+        }
+        return false;
     }
 }
