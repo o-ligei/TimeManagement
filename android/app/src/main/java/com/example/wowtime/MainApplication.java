3ad1abf3e6bba17091d8f451d52278c8d26ac4ee
feaaaaaa <409834725@qq.com>
2020-10-18 16:42:41 +0800

    fix:optimize some imports
    Signed-off-by: feaaaaaa <409834725@qq.com>

--- android/app/src/main/java/com/example/wowtime/MainApplication.java
+++ android/app/src/main/java/com/example/wowtime/MainApplication.java
@@ -1,10 +1,7 @@
 package com.example.wowtime;
 
 import android.app.Application;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 
-import androidx.appcompat.app.AppCompatDelegate;
 
 public class MainApplication extends Application {
     public static final String PACKAGE = "com.example.wowtime";
