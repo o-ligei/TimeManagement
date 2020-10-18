b7df544d68d1be82428089aadc70877bea7247a1
August <46640505+ShaoJieCui@users.noreply.github.com>
2020-10-18 21:08:18 +0800

    feat:communication to get friend list

--- android/app/src/main/java/com/example/wowtime/dto/FriendsListItem.java
+++ android/app/src/main/java/com/example/wowtime/dto/FriendsListItem.java
@@ -5,6 +5,8 @@ public class FriendsListItem {
     private Integer userId;
     private String userIcon;
     private String username;
+    private String phone;
+    private String email;
 
     public FriendsListItem() {
     }
@@ -13,6 +15,16 @@ public class FriendsListItem {
         this.userId = userId;
         this.userIcon = userIcon;
         this.username = username;
+        this.phone = null;
+        this.email = null;
+    }
+
+    public FriendsListItem (Integer userId, String userIcon, String username,String phone,String email){
+        this.userId = userId;
+        this.userIcon = userIcon;
+        this.username = username;
+        this.phone = phone;
+        this.email = email;
     }
 
     public Integer getUserId() {return userId;}
@@ -21,4 +33,8 @@ public class FriendsListItem {
     public void setUserIcon(String userIcon) {this.userIcon = userIcon;}
     public String getUsername() {return username;}
     public void setUsername(String username) {this.username = username;}
+    public String getPhone() {return phone;}
+    public void setPhone(String phone) {this.phone = phone;}
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
 }
