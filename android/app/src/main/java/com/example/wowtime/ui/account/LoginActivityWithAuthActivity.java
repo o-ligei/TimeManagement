46181a928132ffa6c973ff406a21ecae33d18eb8
Cui Shaojie <2633742912@qq.com>
2020-10-19 16:12:03 +0800

    shansimu:communicate with backend (#45)
    * feat: backend (#43) (#44)
    
    Co-authored-by: ziliuziliu <43852461+ziliuziliu@users.noreply.github.com>
    
    * feat:host constant
    
    * feat:user register
    
    * feat:user information
    
    * feat:login with auth
    
    * fix
    
    * fix:number from 1 to 9
    
    * feat:communication in login with password
    
    * feat:communication to get friend list
    
    * fix:convenience for testing
    
    * feat:communication to login with auth
    
    Co-authored-by: ziliuziliu <43852461+ziliuziliu@users.noreply.github.com>
    Co-authored-by: August <46640505+ShaoJieCui@users.noreply.github.com>

--- android/app/src/main/java/com/example/wowtime/ui/account/LoginActivityWithAuthActivity.java
+++ android/app/src/main/java/com/example/wowtime/ui/account/LoginActivityWithAuthActivity.java
@@ -2,26 +2,147 @@ package com.example.wowtime.ui.account;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.view.Menu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.wowtime.R;
+import com.example.wowtime.ui.MainActivity;
+import com.example.wowtime.ui.alarm.ClockSettingActivity;
+import com.example.wowtime.util.InternetConstant;
+import com.example.wowtime.util.UserInfoAfterLogin;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class LoginActivityWithAuthActivity extends AppCompatActivity {
 
+    EditText phoneText;
+    EditText captchaText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_with_auth_activity);
-        TextView usePasswordTextView = findViewById(R.id.textView4);
+        TextView usePasswordTextView = findViewById(R.id.go_to_another_in_auth);
         usePasswordTextView.setOnClickListener(v -> startActivity(new Intent(LoginActivityWithAuthActivity.this, LoginActivityWithPasswordActivity.class)));
-        TextView useRegisterTextView = findViewById(R.id.textView5);
+        TextView useRegisterTextView = findViewById(R.id.go_to_register_in_auth);
         useRegisterTextView.setOnClickListener(v -> startActivity(new Intent(LoginActivityWithAuthActivity.this, RegisterActivity.class)));
+
+        phoneText = findViewById(R.id.phone_input_in_auth);
+        captchaText = findViewById(R.id.captcha_input_in_auth);
+
 //        Button login = findViewById(R.id.button6);
 //        login.setOnClickListener(v -> startActivity(new Intent(LoginActivity1.this, PersonInfo.class)));
+        Button btn_get_captcha = findViewById(R.id.btn_captcha_get_in_auth);
+        btn_get_captcha.setOnClickListener(v -> OKGetCaptcha());
+        Button btn_login = findViewById(R.id.btn_login_in_auth);
+        btn_login.setOnClickListener(v -> OKLoginWitchAuth());
+    }
+
+    private void OKGetCaptcha(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient client = new OkHttpClient();
+                FormBody.Builder formBody = new FormBody.Builder();//创建表单请求体
+                formBody.add("phone",phoneText.getText().toString());//传递键值对参数
+                Request request = new Request.Builder().url(InternetConstant.host + "/User/SendCaptchaToPhone").post(formBody.build()).build();
+                try {
+                    Response response = client.newCall(request).execute();//发送请求
+                    String result = response.body().string();
+                    postCaptcha(result);
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void postCaptcha(String result) throws JSONException {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                JSONObject jsonObject = null;
+                try {
+                    jsonObject = new JSONObject(result);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                String msg = null;
+                try {
+                    msg = jsonObject.get("msg").toString();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Toast toast = Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+    }
+
+    private void OKLoginWitchAuth(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient client = new OkHttpClient();
+                FormBody.Builder formBody = new FormBody.Builder();//创建表单请求体
+                formBody.add("phone",phoneText.getText().toString());
+                formBody.add("captcha",captchaText.getText().toString());
+                Request request = new Request.Builder().url(InternetConstant.host + "/User/LoginWithCaptcha").post(formBody.build()).build();
+                try {
+                    Response response = client.newCall(request).execute();//发送请求
+                    String result = response.body().string();
+                    LoginWitchAuth(result);
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void LoginWitchAuth(String result ) throws JSONException{
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                JSONObject jsonObject = null;
+                try {
+                    jsonObject = new JSONObject(result);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                String msg = null;
+                String str_data = null;
+                String userid = null;
+                try {
+                    msg = jsonObject.get("msg").toString();
+                    str_data = jsonObject.get("data").toString();
+                    JSONObject data = new JSONObject(str_data);
+                    userid = data.get("userid").toString();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Toast toast = Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_SHORT);
+                toast.show();
+                if(msg.equals("success"))
+                {
+                    UserInfoAfterLogin.userid = Integer.valueOf(userid);
+                    Intent intent = new Intent(LoginActivityWithAuthActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
     @Override
---
+++ android/app/src/main/java/com/example/wowtime/util/UserInfoAfterLogin.java
@@ -0,0 +1,5 @@
+package com.example.wowtime.util;
+
+public class UserInfoAfterLogin {
+    public static Integer userid = 1; //set to 1 just for testing
+}
