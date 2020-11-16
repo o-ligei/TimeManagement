7c3f410b3ed39e917d92bc3819d81450bb87fd89
August <46640505+ShaoJieCui@users.noreply.github.com>
2020-11-16 15:16:50 +0800

    feat:add alarm by speech

---
+++ android/app/src/main/java/com/example/wowtime/service/AddAlarm.java
@@ -0,0 +1,64 @@
+package com.example.wowtime.service;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.alibaba.fastjson.JSONObject;
+import com.example.wowtime.dto.AlarmListItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddAlarm {
+    AlarmListItem newAlarm;
+    private Context mContext;
+
+//    public AddAlarm(Context mContext){
+//        this.mContext = mContext;
+//        ArrayList<Boolean> options=new ArrayList<>();
+//        for (int i=0;i<8;i++){
+//            options.add(false);
+//        }
+//        newAlarm=new AlarmListItem("made by voice",options,"使劲摇摇摇","radar",0,0);
+//    }
+
+    public AddAlarm(boolean flag,int weekday,int hour,Context mContext){
+        this.mContext = mContext;
+        ArrayList<Boolean> options=new ArrayList<>();
+        for (int i=0;i<8;i++){
+            options.add(false);
+        }
+        if(flag){
+            options.set(0,true);
+        }
+        else{
+            options.set(weekday,true);
+        }
+        newAlarm=new AlarmListItem("made by voice",options,"使劲摇摇摇","radar",hour,0);
+    }
+
+//    public void setTime(int hour,int minute){
+//        newAlarm.setHour(hour);
+//        newAlarm.setMinute(minute);
+//    }
+//
+//    public void setFrequency(ArrayList<Boolean> options){
+//        newAlarm.setFrequency(options);
+//    }
+
+    public void storeAlarm(){
+        System.out.println("store voice alarm");
+        SharedPreferences mySharedPreferences= mContext.getSharedPreferences("alarmList", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = mySharedPreferences.edit();
+        String shared=mySharedPreferences.getString("list","");
+        List<AlarmListItem> alarmList=new ArrayList<>();
+        if(!shared.equals("") &&shared!=null){
+            alarmList= JSONObject.parseArray(shared,AlarmListItem.class);
+        }
+        alarmList.add(newAlarm);
+        shared=JSONObject.toJSONString(alarmList);
+        editor.putString("list",shared);
+        editor.apply();
+    }
+}
