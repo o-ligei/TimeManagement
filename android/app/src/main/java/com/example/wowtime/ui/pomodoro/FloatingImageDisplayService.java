bdb93da6772efd0714cedb0c35601833809dfc1a
feaaaaaa <409834725@qq.com>
2020-11-16 09:11:37 +0800

    fix: simplify PomodoroSettingActivity
    Signed-off-by: feaaaaaa <409834725@qq.com>

--- android/app/src/main/java/com/example/wowtime/ui/pomodoro/FloatingImageDisplayService.java
+++ android/app/src/main/java/com/example/wowtime/ui/pomodoro/FloatingImageDisplayService.java
@@ -9,14 +9,11 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.provider.Settings;
-
 import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -29,16 +26,16 @@ import java.util.TimerTask;
 
 public class FloatingImageDisplayService extends Service {
     public static boolean isStarted = false;
-    private static boolean isCanceled=false;    //when canceled, cannot be start again
-    private static int time=0;  // notice that it's statistic
+    private static boolean isCanceled = false;    //when canceled, cannot be start again
+    private static int time = 0;  // notice that it's statistic
 
     private WindowManager windowManager;
     private WindowManager.LayoutParams layoutParams;
 
     private View displayView;
 
-    private Timer timingTimer;
-    private TimerTask timingTask;
+    private Timer timingTimer, workTimer, restTimer;
+    private TimerTask timingTask, workTimerTask, restTimerTask;
     private static Handler timingHandler;
 
 
@@ -46,7 +43,7 @@ public class FloatingImageDisplayService extends Service {
     public void onCreate() {
         super.onCreate();
 
-        if(isCanceled) {
+        if (isCanceled) {
             System.out.println("canceled");
             return;
         }
@@ -65,7 +62,8 @@ public class FloatingImageDisplayService extends Service {
         }
         layoutParams.format = PixelFormat.RGBA_8888;
         layoutParams.gravity = Gravity.START | Gravity.TOP;
-        layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                | WindowManager.LayoutParams.FLAG_FULLSCREEN;
         //set width, height, x, y of layoutParams
         WindowManager wm = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
         assert wm != null;
@@ -97,8 +95,14 @@ public class FloatingImageDisplayService extends Service {
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if(!isCanceled)
-            showFloatingWindow();
+        if (!isCanceled) {
+            int work = intent.getIntExtra("work", 0);
+            int rest = intent.getIntExtra("rest", 0);
+            System.out.println("work:" + work);
+            System.out.println("rest:" + rest);
+            if (work != 0) showFloatingWindow(work, rest);
+            else showFloatingWindow();
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -107,7 +111,7 @@ public class FloatingImageDisplayService extends Service {
         if (Settings.canDrawOverlays(this)) {
             LayoutInflater layoutInflater = LayoutInflater.from(this);
             //display screenSaver view
-            displayView=layoutInflater.inflate(R.layout.screen_saver_activity,null);
+            displayView = layoutInflater.inflate(R.layout.screen_saver_activity, null);
 //            displayView = layoutInflater.inflate(R.layout.image_display, null);
 //            displayView.setOnTouchListener(new FloatingOnTouchListener());
 //            ImageView imageView = displayView.findViewById(R.id.image_display_imageview);
@@ -116,64 +120,139 @@ public class FloatingImageDisplayService extends Service {
 //            View viewbar = new customViewGroup(this);
 //            windowManager.addView(viewbar, layoutParams);
 
-            //implement going time
-            TextView textView=displayView.findViewById(R.id.saver_gone_time);
-            timingTimer = new Timer();
-            timingHandler=new Handler(new Handler.Callback() {
-                @Override
-                public boolean handleMessage(Message msg) {
-                    switch (msg.what) {
-                        case 1:
-                            ++time;
-                            textView.setText(secondToTime(time));
-                    }
-                    return false;
-                }
-            });
-            timingTask = new TimerTask() {
-                @Override
-                public void run() {
-                    Message message = timingHandler.obtainMessage(1);
-                    timingHandler.sendMessage(message);
-                }
-            };
-
             //white list button
-            Button whitelist=displayView.findViewById(R.id.white_list_button);
-            whitelist.setOnClickListener(v->{
+            Button whitelist = displayView.findViewById(R.id.white_list_button);
+            whitelist.setOnClickListener(v -> {
                 windowManager.removeView(displayView);
                 System.out.println("FloatingImageDisplayService: go to whitelist");
-                Intent intent=new Intent(getApplicationContext(),WhiteListActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK );
-                intent.putExtra("fromScreenSaver",1);
-               startActivity(intent);
+                Intent intent = new Intent(getApplicationContext(), WhiteListActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.putExtra("fromScreenSaver", 1);
+                startActivity(intent);
             });
 
-            if(windowManager==null) {
+            if (windowManager == null) {
                 //something cannot understand happened
                 System.out.println("wm is null?");
                 onCreate();
             }
 
-
             //display and begin timing
-            windowManager.addView(displayView, layoutParams);
-            timingTimer.schedule(timingTask, 0, 1000);
+//            windowManager.addView(displayView, layoutParams);
+            System.out.println("preparation of floatingWindow has been ok");
         }
     }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void showFloatingWindow(int workTime, int restTime) {
+        showFloatingWindow();
+
+        Handler workHandler = new Handler(), restHanlder = new Handler();
+        workTimer = new Timer();
+        restTimer = new Timer();
+
+        workTimerTask = new TimerTask() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void run() {
+                System.out.println("start floating window");
+                workHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        windowManager.addView(displayView, layoutParams);
+//                        System.out.println("stop");
+//                        int SCAN_INTERVAL = 1000;
+//                        for (int i = 0; i < rest / SCAN_INTERVAL - 2; i++) {
+//                            String currentApp = ApkTool.getTaskPackname(PomodoroSettingActivity.this);
+//                            System.out.println("Current Runnning: " + currentApp);
+//                            if (currentApp.equals("CurrentNULL"))
+//                                startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
+//                            if (currentApp.equals("com.netease.cloudmusic"))
+//                                runOnUiThread(() -> {
+//                                    startFloatingImageDisplayService(buttonBegin);
+//                                });
+//                            try {
+//                                Thread.sleep(SCAN_INTERVAL);
+//                            } catch (InterruptedException e) {
+//                                e.printStackTrace();
+//                            }
+//                        }
+                    }
+                });
+            }
+        };
+
+        restTimerTask = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("remove floating window");
+                restHanlder.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        windowManager.removeView(displayView);
+                    }
+                });
+            }
+        };
+
+        //implement going time
+        timingTimer = new Timer();
+        timingHandler = new Handler(new Handler.Callback() {
+            TextView textView;
+
+            @Override
+            public boolean handleMessage(Message msg) {
+                switch (msg.what) {
+                    case 1:
+                        ++time;
+                        if (textView == null && displayView != null)
+                            textView = displayView.findViewById(R.id.saver_gone_time);
+                        if (textView != null)
+                            textView.setText(secondToTime(time));
+                }
+                return false;
+            }
+        });
+        timingTask = new TimerTask() {
+            @Override
+            public void run() {
+                Message message = timingHandler.obtainMessage(1);
+                timingHandler.sendMessage(message);
+            }
+        };
+
+        workTimer.schedule(workTimerTask, 0, restTime + workTime);
+        restTimer.schedule(restTimerTask, workTime, workTime + restTime);
+        timingTimer.schedule(timingTask, 0, 1000);
+        System.out.println("begin 3 timers in floatingDisplayService");
+    }
+
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        isStarted=false;
+
+        isStarted = false;
+
         timingTimer.cancel();
         timingTask.cancel();
-        System.out.println("destroy");
-        windowManager.removeView(displayView);
+        workTimer.cancel();
+        workTimerTask.cancel();
+        restTimer.cancel();
+        restTimerTask.cancel();
+
+        try {
+            windowManager.removeView(displayView);
+        } catch (Exception e) {
+            System.out.println("it's in rest\n" + e.toString());
+        }
+
+        System.out.println("onDestroy finish");
     }
 
     public static int setIsCanceled(boolean flag) {
-        isCanceled=flag;
+        isCanceled = flag;
         return time;
     }
 
@@ -182,10 +261,10 @@ public class FloatingImageDisplayService extends Service {
     }
 
 
-    public static String secondToTime(long second){
+    public static String secondToTime(long second) {
         long hours = second / 3600;            //转换小时
         second = second % 3600;                //剩余秒数
-        long minutes = second /60;            //转换分钟
+        long minutes = second / 60;            //转换分钟
         second = second % 60;                //剩余秒数
         return hours + ":" + minutes + ":" + second;
     }
