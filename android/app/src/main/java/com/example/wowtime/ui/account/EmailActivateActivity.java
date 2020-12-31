5d428d0d996a3ee084df468d19897b9d77680797
ShanSimu <2633742912@qq.com>
2020-12-31 15:15:11 +0800

    fix:modify ajax handler

--- android/app/src/main/java/com/example/wowtime/ui/account/EmailActivateActivity.java
+++ android/app/src/main/java/com/example/wowtime/ui/account/EmailActivateActivity.java
@@ -2,12 +2,14 @@ package com.example.wowtime.ui.account;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.wowtime.R;
 import com.example.wowtime.ui.MainActivity;
+import com.example.wowtime.util.Ajax;
 import com.example.wowtime.util.InternetConstant;
 import java.io.IOException;
 import okhttp3.FormBody;
@@ -74,40 +76,14 @@ public class EmailActivateActivity extends AppCompatActivity {
     }
 
     void OkHandsOn() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                OkHttpClient client = new OkHttpClient();
-                FormBody.Builder formBody = new FormBody.Builder();
-                formBody.add("phone", phone);
-                formBody.add("email", emailInput.getText().toString());
-                formBody.add("captcha", captchaInput.getText().toString());
-                Request request = new Request.Builder()
-                        .url(InternetConstant.host + "/User/ActivateEmail").post(formBody.build())
-                        .build();
-                try {
-                    Response response = client.newCall(request).execute();//发送请求
-                    String result = response.body().string();
-                    HandsOn(result);
-                } catch (IOException | JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-    }
+        FormBody.Builder formBody = new FormBody.Builder();
+        formBody.add("phone", phone);
+        formBody.add("email", emailInput.getText().toString());
+        formBody.add("captcha", captchaInput.getText().toString());
 
-    void HandsOn(String result) throws JSONException {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                JSONObject jsonObject = null;
-                String msg = "";
-                try {
-                    jsonObject = new JSONObject(result);
-                    msg = jsonObject.get("msg").toString();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+        Handler handler = new Handler(message -> {
+            if(message.what == InternetConstant.FETCH){
+                String msg = message.getData().get("msg").toString();
                 Toast toast = Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT);
                 toast.show();
                 if (msg.equals("success")) {
@@ -115,8 +91,53 @@ public class EmailActivateActivity extends AppCompatActivity {
                     intent = new Intent(EmailActivateActivity.this, MainActivity.class);
                     startActivity(intent);
                     finish();
-                }
-            }
+                }}
+            return false;
         });
+        Ajax ajax = new Ajax("/User/ActivateEmail",formBody,handler,InternetConstant.FETCH);
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                OkHttpClient client = new OkHttpClient();
+//
+//
+//
+//
+//                Request request = new Request.Builder()
+//                        .url(InternetConstant.host + "/User/ActivateEmail").post(formBody.build())
+//                        .build();
+//                try {
+//                    Response response = client.newCall(request).execute();//发送请求
+//                    String result = response.body().string();
+//                    HandsOn(result);
+//                } catch (IOException | JSONException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }).start();
     }
+
+//    void HandsOn(String result) throws JSONException {
+//        runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                JSONObject jsonObject = null;
+//                String msg = "";
+//                try {
+//                    jsonObject = new JSONObject(result);
+//                    msg = jsonObject.get("msg").toString();
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//                Toast toast = Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT);
+//                toast.show();
+//                if (msg.equals("success")) {
+//                    Intent intent = null;
+//                    intent = new Intent(EmailActivateActivity.this, MainActivity.class);
+//                    startActivity(intent);
+//                    finish();
+//                }
+//            }
+//        });
+//    }
 }
--- android/app/src/main/java/com/example/wowtime/ui/account/RegisterActivity.java
+++ android/app/src/main/java/com/example/wowtime/ui/account/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.wowtime.ui.account;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,7 @@ import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.JSONObject;
 import com.example.wowtime.R;
 import com.example.wowtime.ui.alarm.TaskSuccessActivity;
+import com.example.wowtime.util.Ajax;
 import com.example.wowtime.util.InternetConstant;
 import java.io.IOException;
 
@@ -100,45 +102,25 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void OKRegister() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                OkHttpClient client = new OkHttpClient();
-                FormBody.Builder formBody = new FormBody.Builder();//创建表单请求体
-                formBody.add("username", useNameTextView.getText().toString());
-                formBody.add("password", passwordTextView.getText().toString());
-                formBody.add("phone", phoneTextView.getText().toString());//传递键值对参数
-                formBody.add("captcha", captchaTextView.getText().toString());
-                Request request = new Request.Builder()
-                        .url(InternetConstant.host + "/User/Register").post(formBody.build())
-                        .build();
-                try {
-                    Response response = client.newCall(request).execute();//发送请求
-                    String result = response.body().string();
-                    System.out.println(result);
-                    postRegister(result);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-    }
+        FormBody.Builder formBody = new FormBody.Builder();//创建表单请求体
+        formBody.add("username", useNameTextView.getText().toString());
+        formBody.add("password", passwordTextView.getText().toString());
+        formBody.add("phone", phoneTextView.getText().toString());//传递键值对参数
+        formBody.add("captcha", captchaTextView.getText().toString());
 
-    private void postRegister(String result) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                JSONObject jsonObject = null;
-                String msg = null;
-                try {
-//                    jsonObject = new JSONObject(Boolean.parseBoolean(result));
-                    jsonObject = JSONObject.parseObject(result);
-                    System.out.println(jsonObject);
-                    msg = jsonObject.get("msg").toString();
-                    System.out.println(msg);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+        Handler handler = new Handler(message -> {
+            if(message.what == InternetConstant.FETCH){
+//                JSONObject jsonObject = null;
+                String msg = message.getData().get("msg").toString();
+//                try {
+////                    jsonObject = new JSONObject(Boolean.parseBoolean(result));
+//                    jsonObject = JSONObject.parseObject(result);
+//                    System.out.println(jsonObject);
+//                    msg = jsonObject.get("msg").toString();
+//                    System.out.println(msg);
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
 
                 Toast toast = Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT);
                 toast.show();
@@ -149,8 +131,62 @@ public class RegisterActivity extends AppCompatActivity {
                     finish();
                 }
             }
+            return false;
         });
+
+        Ajax ajax = new Ajax("/User/Register",formBody,handler,InternetConstant.FETCH);
+        ajax.fetch();
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                OkHttpClient client = new OkHttpClient();
+//                FormBody.Builder formBody = new FormBody.Builder();//创建表单请求体
+//                formBody.add("username", useNameTextView.getText().toString());
+//                formBody.add("password", passwordTextView.getText().toString());
+//                formBody.add("phone", phoneTextView.getText().toString());//传递键值对参数
+//                formBody.add("captcha", captchaTextView.getText().toString());
+//                Request request = new Request.Builder()
+//                        .url(InternetConstant.host + "/User/Register").post(formBody.build())
+//                        .build();
+//                try {
+//                    Response response = client.newCall(request).execute();//发送请求
+//                    String result = response.body().string();
+//                    System.out.println(result);
+//                    postRegister(result);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }).start();
     }
+
+//    private void postRegister(String result) {
+//        runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                JSONObject jsonObject = null;
+//                String msg = null;
+//                try {
+////                    jsonObject = new JSONObject(Boolean.parseBoolean(result));
+//                    jsonObject = JSONObject.parseObject(result);
+//                    System.out.println(jsonObject);
+//                    msg = jsonObject.get("msg").toString();
+//                    System.out.println(msg);
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                Toast toast = Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT);
+//                toast.show();
+//                if (msg.equals("success")) {
+//                    Intent intent = new Intent(RegisterActivity.this,
+//                                               LoginActivityWithPasswordActivity.class);
+//                    startActivity(intent);
+//                    finish();
+//                }
+//            }
+//        });
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
