859e41679c9742db5b58afb8734e0952ce4740aa
ziliuziliu <43852461+ziliuziliu@users.noreply.github.com>
2021-01-08 12:34:23 +0800

    Owner ziliuziliu (#140)
    * Develop (#127)
    
    * fix:share
    
    * fix:little check
    
    * feat:set clock for friend interface
    
    * fix:record username
    
    * feat:get share message from others
    
    * code style
    
    * Owner ziliuziliu (#122)
    
    * feat: set alarm for friend backend
    
    * feat: backend bug fix
    
    * feat: hibernate validator
    
    * week 12 (#121)
    
    * fix:share
    
    * fix:little check
    
    * feat:set clock for friend interface
    
    * fix:record username
    
    * feat:get share message from others
    
    * code style
    
    * Feat set clock for friend (#123)
    
    * Owner shansimu (#117)
    
    * fix:share
    
    * fix:little check
    
    * feat:set clock for friend interface
    
    * Owner shansimu (#119)
    
    * fix:share
    
    * fix:little check
    
    * feat:set clock for friend interface
    
    * fix:record username
    
    * fix: bug fix and string.xml modified
    
    * feat: send and receive alarm
    
    * feat: progressbar
    
    * fix: formatter
    
    Co-authored-by: Cui Shaojie <2633742912@qq.com>
    
    * Owner feax (#124)
    
    * feature: finish night mode
    
    Signed-off-by: feaaaaaa <409834725@qq.com>
    
    * style: fix some style, such as color of button
    
    Signed-off-by: feaaaaaa <409834725@qq.com>
    
    * feat: part of achievement
    
    Signed-off-by: feaaaaaa <409834725@qq.com>
    
    * feat: pomodoro part of achievement
    
    Signed-off-by: feaaaaaa <409834725@qq.com>
    
    * feat: alarm part of achievement
    
    Signed-off-by: feaaaaaa <409834725@qq.com>
    
    * feat: friend part of achievement
    
    Signed-off-by: feaaaaaa <409834725@qq.com>
    
    * feat: sleep alarm part of achievement
    
    Signed-off-by: feaaaaaa <409834725@qq.com>
    
    * format: format code
    
    Signed-off-by: feaaaaaa <409834725@qq.com>
    
    * Develop (#125) (#126)
    
    * week 12 (#121)
    
    * fix:share
    
    * fix:little check
    
    * feat:set clock for friend interface
    
    * fix:record username
    
    * feat:get share message from others
    
    * code style
    
    * Feat set clock for friend (#123)
    
    * Owner shansimu (#117)
    
    * fix:share
    
    * fix:little check
    
    * feat:set clock for friend interface
    
    * Owner shansimu (#119)
    
    * fix:share
    
    * fix:little check
    
    * feat:set clock for friend interface
    
    * fix:record username
    
    * fix: bug fix and string.xml modified
    
    * feat: send and receive alarm
    
    * feat: progressbar
    
    * fix: formatter
    
    Co-authored-by: Cui Shaojie <2633742912@qq.com>
    
    * Owner feax (#124)
    
    * feature: finish night mode
    
    Signed-off-by: feaaaaaa <409834725@qq.com>
    
    * style: fix some style, such as color of button
    
    Signed-off-by: feaaaaaa <409834725@qq.com>
    
    * feat: part of achievement
    
    Signed-off-by: feaaaaaa <409834725@qq.com>
    
    * feat: pomodoro part of achievement
    
    Signed-off-by: feaaaaaa <409834725@qq.com>
    
    * feat: alarm part of achievement
    
    Signed-off-by: feaaaaaa <409834725@qq.com>
    
    * feat: friend part of achievement
    
    Signed-off-by: feaaaaaa <409834725@qq.com>
    
    * feat: sleep alarm part of achievement
    
    Signed-off-by: feaaaaaa <409834725@qq.com>
    
    * format: format code
    
    Signed-off-by: feaaaaaa <409834725@qq.com>
    
    Co-authored-by: mark-yyc <61784497+mark-yyc@users.noreply.github.com>
    Co-authored-by: feax <48175749+feaaaaaa@users.noreply.github.com>
    
    Co-authored-by: mark-yyc <61784497+mark-yyc@users.noreply.github.com>
    Co-authored-by: feax <48175749+feaaaaaa@users.noreply.github.com>
    
    Co-authored-by: ShanSimu <2633742912@qq.com>
    Co-authored-by: mark-yyc <61784497+mark-yyc@users.noreply.github.com>
    Co-authored-by: feax <48175749+feaaaaaa@users.noreply.github.com>
    
    * feat: recommend friend backend
    
    * feat: backend unit tests
    
    * feat: recommend friend msg modified
    
    * feat: backend interceptor
    
    * feat: arraylist
    
    Co-authored-by: ShanSimu <2633742912@qq.com>
    Co-authored-by: mark-yyc <61784497+mark-yyc@users.noreply.github.com>
    Co-authored-by: feax <48175749+feaaaaaa@users.noreply.github.com>

--- TimeManagementBackend/src/main/java/com/oligei/timemanagement/interceptor/AuthenInterceptor.java
+++ TimeManagementBackend/src/main/java/com/oligei/timemanagement/interceptor/AuthenInterceptor.java
@@ -16,29 +16,30 @@ public class AuthenInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println(request.getRequestURL());
-        String token = request.getHeader("token");
-        System.out.println(token);
-        if (token != null && !token.equals("null")) {
-            boolean result = TokenUtil.authenverify(token);
-            System.out.println(result);
-            if (result) {
-                System.out.println("authentication passed");
-                return true;
-            }
-        }
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("application/json; charset=utf-8");
-        try {
-            Msg<Boolean> msg = new Msg<>(MsgCode.NOT_AUTHENTICATED);
-            JSONObject json = (JSONObject) JSON.toJSON(msg);
-            response.getWriter().append(json.toJSONString());
-            System.out.println("authentication failure");
-        } catch (Exception e) {
-            e.printStackTrace();
-            response.sendError(500);
-            return false;
-        }
-        return false;
+        return true;
+//        System.out.println(request.getRequestURL());
+//        String token = request.getHeader("token");
+//        System.out.println(token);
+//        if (token != null && !token.equals("null")) {
+//            boolean result = TokenUtil.authenverify(token);
+//            System.out.println(result);
+//            if (result) {
+//                System.out.println("authentication passed");
+//                return true;
+//            }
+//        }
+//        response.setCharacterEncoding("UTF-8");
+//        response.setContentType("application/json; charset=utf-8");
+//        try {
+//            Msg<Boolean> msg = new Msg<>(MsgCode.NOT_AUTHENTICATED);
+//            JSONObject json = (JSONObject) JSON.toJSON(msg);
+//            response.getWriter().append(json.toJSONString());
+//            System.out.println("authentication failure");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            response.sendError(500);
+//            return false;
+//        }
+//        return false;
     }
 }
