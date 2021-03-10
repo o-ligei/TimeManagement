1ea62acfefdeb057a2042df6360fbd866f4e5f27
feax <409834725@qq.com>
2021-03-10 23:56:33 +0800

    clean

--- finalRelease/code/backend/src/main/java/com/oligei/timemanagement/exceptions/NotOnlineException.java
+++
@@ -1,6 +0,0 @@
-package com.oligei.timemanagement.exceptions;
-
-public class NotOnlineException extends Exception {
-
-    public NotOnlineException(String msg) {super(msg);}
-}
--- finalRelease/code/frontend/app/src/main/java/com/example/wowtime/util/UserInfoAfterLogin.java
+++
@@ -1,8 +0,0 @@
-package com.example.wowtime.util;
-
-public class UserInfoAfterLogin {
-
-    public static Integer userid = -1; //set to -1 by default
-    public static String username = "";
-    public static boolean webSocketMessage = false;
-}
